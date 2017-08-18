a4ca912a86c516c3c8ea3a19f2fa35e536304044
guillendiaz <guillendiazd@unitec.edu>
2017-08-18 16:00:56 -0600

    Otra actualizacion

---
+++ Targaryen.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Familia.h"
+#include "Dothraki.h"
+#include "Dragones.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Targaryen : public Familia{
+	private:
+		int Barcos;
+		vector<Dothraki*> vDoth;
+		vector<Dragones*> vDrag;
+	public:
+		int getBarcos();
+		vector<Dothraki*> getDoth();
+		vector<Dragones*> getDrag();
+		void setBarcos(int);
+};
