eb164f0c37d75b5cde54ff65f607dbdf157bf956
guillendiaz <guillendiazd@unitec.edu>
2017-08-18 13:48:06 -0600

    Inicio

---
+++ Starks.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Starks{
+	private:
+		Starks();
+		Starks(string, int, string, string, string, int);
+		string JefeFamilia;
+		int CantidadLobosHuargos;
+		string AnimalEmblema;
+		string Lema;
+		PequeniaFamiliaNoble* Ejercito;
+		string GuerreroValioso;
+		int CantidadIntegrantes;
+	public:
+
+
+};
